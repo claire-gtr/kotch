c004fab4549942ee0d0068f4ce819e58d4fb41f3
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-09-13 17:42:56 +0200

    add credits

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,6 @@ class StripeCheckoutSessionService
   def call(event)
     pack_order = PackOrder.find_by(checkout_session_id: event.data.object.id)
     pack_order.update(state: 'paid')
+    pack_order.user.update(credit_count: (pack_order.user.credit_count + pack_order.credit_count))
   end
 end

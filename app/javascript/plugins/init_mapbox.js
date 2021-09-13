203bf2c6553f38e367e7857efcc331c6fd6d013d
claire gautier <claire.gautier@edhec.com>
2021-09-13 15:45:48 +0200

    fixed map + content faq

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -23,7 +23,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 12 });
 };
 
 const initMapbox = () => {
@@ -31,9 +31,12 @@ const initMapbox = () => {
   if (mapElement) {
     const map = buildMap(mapElement);
     const markers = JSON.parse(mapElement.dataset.markers);
+    map.on('idle', function () { map.resize() });
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
   }
 };
+
+
 
 export { initMapbox };

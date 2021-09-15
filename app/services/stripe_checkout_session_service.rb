50ddf5de143adc31d3e35ade3f3ab3da0f0c9e91
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-09-15 14:50:24 +0200

    added control for checkout webhook

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,8 +1,10 @@
 class StripeCheckoutSessionService
   def call(event)
-    pack_order = PackOrder.find_by(checkout_session_id: event.data.object.id)
-    pack_order.update(state: 'paid')
-    pack_order.user.update(credit_count: (pack_order.user.credit_count + pack_order.credit_count))
-    StripeMailer.with(user: pack_order.user, pack_order: pack_order).customer_bought_credits.deliver_now
+    if event.data.object.mode == "payment"
+      pack_order = PackOrder.find_by(checkout_session_id: event.data.object.id)
+      pack_order.update(state: 'paid')
+      pack_order.user.update(credit_count: (pack_order.user.credit_count + pack_order.credit_count))
+      StripeMailer.with(user: pack_order.user, pack_order: pack_order).customer_bought_credits.deliver_now
+    end
   end
 end

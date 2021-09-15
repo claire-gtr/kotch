4cc92f39f223cab391ecfe53fd50327134b4f7a9
claire gautier <claire.gautier@edhec.com>
2021-09-15 15:30:46 +0200

    redirect after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations"}
   root to: 'pages#home'
   get '/mon-profil', to: 'users#profile', as: :profile
   resources :lessons, only: [:index, :new, :create]  do

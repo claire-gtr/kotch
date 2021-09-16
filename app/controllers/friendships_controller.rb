e79972b899450edfd70b37f6de92d62f5a3e8f02
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-16 18:18:44 +0200

    new friends search backend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,15 +6,23 @@ class FriendshipsController < ApplicationController
     @my_friend_requests_as_requestor = FriendRequest.where(requestor: current_user).where(status: "pending")
     @my_friend_requests_as_receiver = FriendRequest.where(receiver: current_user).where(status: "pending")
 
-    if params[:query].present?
+    if params[:friends_query].present?
       @my_friends = current_user.friendships.select do |friendship|
-        friendship.friend_a.first_name.downcase == params[:query].downcase || 
-        friendship.friend_b.first_name.downcase == params[:query].downcase ||
-        friendship.friend_a.last_name.downcase == params[:query].downcase || 
-        friendship.friend_b.last_name.downcase == params[:query].downcase
+        friendship.friend_a.first_name.downcase == params[:friend_query].downcase || 
+        friendship.friend_b.first_name.downcase == params[:friend_query].downcase ||
+        friendship.friend_a.last_name.downcase == params[:friend_query].downcase || 
+        friendship.friend_b.last_name.downcase == params[:friend_query].downcase
       end
     else 
       @my_friends = current_user.friendships
+    end
+
+    if params[:new_friend_query].present?
+      @new_friends = User.all.select do |user|
+        user.email.downcase == params[:new_friend_query].downcase
+      end
+    else
+      @new_friends = []
     end
   end
 end

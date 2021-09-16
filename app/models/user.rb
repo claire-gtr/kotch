1f4ad7c6c6123da2c35cd75f17819b087f968383
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-09-16 10:26:24 +0200

    handle no coupon

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,8 +100,8 @@ class User < ApplicationRecord
     partners_domains = Partner.all.pluck(:domain_name)
     if partners_domains.include?(domain_to_check)
       partner = Partner.find_by(domain_name: domain_to_check)
-      return partner.coupon_code
+      return {exist: true, code: partner.coupon_code}
     end
-    return ""
+    {exist: false, code: nil}
   end
 end

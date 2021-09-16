e299314fd981ee0ce4ea52b012ccb05a8276462f
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-16 17:17:55 +0200

    search backend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,6 +5,16 @@ class FriendshipsController < ApplicationController
     @friend_request = FriendRequest.new
     @my_friend_requests_as_requestor = FriendRequest.where(requestor: current_user).where(status: "pending")
     @my_friend_requests_as_receiver = FriendRequest.where(receiver: current_user).where(status: "pending")
-    @my_friends = current_user.friendships
+
+    if params[:query].present?
+      @my_friends = current_user.friendships.select do |friendship|
+        friendship.friend_a.first_name.downcase == params[:query].downcase || 
+        friendship.friend_b.first_name.downcase == params[:query].downcase ||
+        friendship.friend_a.last_name.downcase == params[:query].downcase || 
+        friendship.friend_b.last_name.downcase == params[:query].downcase
+      end
+    else 
+      @my_friends = current_user.friendships
+    end
   end
 end

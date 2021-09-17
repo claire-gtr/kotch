2a9a1ed7eaee0b78c0f7281b4ad9ec23c7b6126b
claire gautier <claire.gautier@edhec.com>
2021-09-17 15:58:47 +0200

    error pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,10 @@ claire = User.new(
 claire.save
 
 nico = User.new(
-  email: "nico@gmail.com",
+  email: "nicolasvandenbussche0@gmail.com",
   password: 'password',
   first_name: 'nico',
-  last_name: 'vandenbussche',
-  admin: true)
+  last_name: 'vandenbussche')
 nico.save
 
 noemie = User.new(
@@ -49,52 +48,3 @@ corentin = User.new(
   last_name: 'grandin',
   admin: true)
 corentin.save
-test1 = User.new(
-  email: "test1@gmail.com",
-  password: 'password',
-  first_name: 'test1',
-  last_name: 'test',
-  admin: true)
-test1.save
-test2 = User.new(
-  email: "test2@gmail.com",
-  password: 'password',
-  first_name: 'test2',
-  last_name: 'test',
-  admin: true)
-test2.save
-test3 = User.new(
-  email: "test3@gmail.com",
-  password: 'password',
-  first_name: 'test3',
-  last_name: 'test',
-  admin: true)
-test3.save
-test4 = User.new(
-  email: "test4@gmail.com",
-  password: 'password',
-  first_name: 'test4',
-  last_name: 'test',
-  admin: true)
-test4.save
-test5 = User.new(
-  email: "test5@gmail.com",
-  password: 'password',
-  first_name: 'test5',
-  last_name: 'test',
-  admin: true)
-test5.save
-
-coach = User.new(
-  email: "coach@gmail.com",
-  password: 'password',
-  first_name: 'coach',
-  last_name: 'sportif',
-  coach: true)
-coach.save
-
-FriendRequest.create(requestor: test1, receiver: corentin)
-FriendRequest.create(requestor: test2, receiver: corentin)
-FriendRequest.create(requestor: corentin, receiver: test4)
-FriendRequest.create(requestor: corentin, receiver: test3)
-Friendship.create(friend_a: nico, friend_b: corentin)

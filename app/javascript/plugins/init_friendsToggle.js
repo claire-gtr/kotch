6f8bd3d1534cdce8896ecc328cae7cdf5e62548a
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-17 10:19:24 +0200

    remove unused

--- app/javascript/plugins/init_friendsToggle.js
+++
@@ -1,6 +0,0 @@
-tab = document.querySelector('.friends-tab');
-form = document.querySelector('.friends-form');
-
-tab.classList.add('friends-tab-result');
-tab.classList.add('shadow');
-form.classList.remove('shadow');

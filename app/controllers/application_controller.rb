b30edcfec4a4e9d19c3049486969422f0c6b9013
claire gautier <claire.gautier@edhec.com>
2021-09-19 15:47:44 +0200

    leags optin and meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :address, :phone_number, :birth_date, :gender, :sport_habits, :level, :intensity, :expectations, :physical_pain, :coach, :terms])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :address, :phone_number, :birth_date, :gender, :sport_habits, :level, :intensity, :expectations, :physical_pain, :coach, :terms, :optin_cgv])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar, :address, :phone_number, :birth_date, :gender, :sport_habits, :level, :intensity, :expectations, :physical_pain, :terms])

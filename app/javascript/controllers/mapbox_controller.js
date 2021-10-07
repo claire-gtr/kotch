c5afecc9406d2908559d0d4237069be5da2c69cc
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-10-07 17:05:57 +0200

    select location visible true from mapbox is OK

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,15 +1,40 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["collection", "address", "modal"]
+  static targets = ["collection", "address", "modal", "container"]
 
   locationSelect(event) {
     const locationID = event.currentTarget.dataset.id;
-    this.modalTarget.style.display = "none";
-    this.addressTarget.classList.add('hidden');
-    this.collectionTarget.classList.remove('hidden');
+    const algoliaPlaces = document.querySelector('.algolia-places');
+    const body = document.querySelector('body');
+    const backdrop = document.querySelector('.modal-backdrop');
 
-    console.log();
+    this.modalTarget.setAttribute('aria-hidden', 'true');
+    this.modalTarget.removeAttribute('aria-modal');
+    this.modalTarget.removeAttribute('role');
+    this.modalTarget.style.display = 'none';
+    this.modalTarget.classList.remove('show');
+    body.classList.remove('modal-open');
+    body.style = '';
+    backdrop.remove();
+
+    // window.click();
+    // this.containerTarget.click();
+
+    this.addressTarget.disabled = true;
+    this.addressTarget.removeAttribute('placeholder');
+    // this.addressTarget.classList.add('disabled');
+    // algoliaPlaces.classList.add('hidden');
+    // algoliaPlaces.remove();
+
+    // this.collectionTarget.classList.remove('hidden');
+    this.collectionTarget.disabled = false;
+    this.collectionTarget.value = locationID;
   }
 
+  addressSelect() {
+    // this.addressTarget.removeAttribute('disabled');
+    // this.collectionTarget.disabled = true;
+    // this.collectionTarget.value = '';
+  }
 }

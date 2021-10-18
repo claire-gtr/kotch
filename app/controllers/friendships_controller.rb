a1c02c1c06df66fe4ec59aa9c2b142c6b6e1d9b9
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-10-18 13:02:37 +0200

    add error message when search user render nothing

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,4 @@
 class FriendshipsController < ApplicationController
-
   def index
     @friendships = policy_scope(Friendship)
     @friend_request = FriendRequest.new
@@ -13,6 +12,9 @@ class FriendshipsController < ApplicationController
         friendship.friend_a.last_name.downcase == params[:friend_query].downcase.gsub(/\s+/, '') ||
         friendship.friend_b.last_name.downcase == params[:friend_query].downcase.gsub(/\s+/, '')
       end
+      if @my_friends.empty?
+        flash[:notice] = "Aucun compte utilisateur ne correspond ou bien l'orthographe de vos informations est à modifier."
+      end
     else
       @my_friends = current_user.friendships
     end
@@ -21,10 +23,16 @@ class FriendshipsController < ApplicationController
       @new_friends = User.all.select do |user|
         user.email.downcase == params[:email].downcase.gsub(/\s+/, '')
       end
+      if @new_friends.empty?
+        flash[:notice] = "Aucun compte utilisateur ne correspond ou bien l'orthographe de vos informations est à modifier."
+      end
     elsif params[:first_name].present? && params[:last_name].present?
       @new_friends = User.all.select do |user|
-        (user.first_name.downcase == params[:first_name].downcase).gsub(/\s+/, '') &&
-        (user.last_name.downcase == params[:last_name].downcase).gsub(/\s+/, '')
+        (user.first_name.downcase == params[:first_name].downcase.gsub(/\s+/, '')) &&
+        (user.last_name.downcase == params[:last_name].downcase.gsub(/\s+/, ''))
+      end
+      if @new_friends.empty?
+        flash[:notice] = "Aucun compte utilisateur ne correspond ou bien l'orthographe de vos informations est à modifier."
       end
     else
       @new_friends = []

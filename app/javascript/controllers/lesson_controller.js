f9dc5d956acfecd433ff3238fedf2c440edc930f
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-10-21 17:41:22 +0200

    my reservations mobile as customer is finished

--- app/javascript/controllers/lesson_controller.js
+++ app/javascript/controllers/lesson_controller.js
@@ -6,16 +6,24 @@ export default class extends Controller {
   down(event) {
     const lessonID = event.currentTarget.dataset.id;
     const chevronUp = document.querySelector(`.chevronUp-${lessonID}`);
+    const hiddenBtn = document.querySelector(`.hidden-lesson-btn-${lessonID}`);
+    const hiddenBtnCancel = document.querySelector(`.hidden-lesson-btn-cancel-${lessonID}`);
 
     event.currentTarget.classList.add('hidden');
     chevronUp.classList.remove('hidden');
+    hiddenBtn.classList.remove('hidden');
+    hiddenBtnCancel.classList.remove('hidden');
   }
 
-  up() {
+  up(event) {
     const lessonID = event.currentTarget.dataset.id;
     const chevronDown = document.querySelector(`.chevronDown-${lessonID}`);
+    const hiddenBtn = document.querySelector(`.hidden-lesson-btn-${lessonID}`);
+    const hiddenBtnCancel = document.querySelector(`.hidden-lesson-btn-cancel-${lessonID}`);
 
     event.currentTarget.classList.add('hidden');
     chevronDown.classList.remove('hidden');
+    hiddenBtn.classList.add('hidden');
+    hiddenBtnCancel.classList.add('hidden');
   }
 }

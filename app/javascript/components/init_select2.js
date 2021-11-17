4e578cdcb78b6b2bbd45aa2174919342097213b7
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-11-17 10:21:44 +0100

    better css for lessons new

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    placeholder: "Sélectionnez des amis"
+  });
 };
 
 export { initSelect2 };

9bd9dd7841f009292a270ba5470ac5bbc296e506
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-11-25 14:59:23 +0100

    details

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -9,34 +9,16 @@ export default class extends Controller {
 
   toggleSubscription (event) {
     event.stopImmediatePropagation();
-    // event.preventDefault();
 
     const sessionID = event.currentTarget.dataset.sessionId;
     const stripeButton = document.querySelector(`.button-${sessionID}`);
     const cgvCheckbox = document.querySelector(`.checkbox-${sessionID}`);
     const cgvText = document.querySelector(`.cgv-text-${sessionID}`);
-    console.log(cgvCheckbox.checked)
+    console.log(event.currentTarget.checked);
+    console.log(cgvCheckbox.checked);
     if (event.currentTarget === cgvCheckbox) {
-      cgvCheckbox.checked = !cgvCheckbox.checked;
+      event.currentTarget.checked = !event.currentTarget.checked;
     } else if (event.currentTarget === stripeButton && cgvCheckbox.checked === true) {
-      let stripe = Stripe("<%= ENV["STRIPE_PUBLISHABLE_KEY"] %>");
-      let checkoutButtons = document.querySelectorAll('.checkout-button');
-      checkoutButtons.forEach(checkoutButton => {
-        checkoutButton.addEventListener('click', function () {
-          stripe.redirectToCheckout({
-            sessionId: checkoutButton.dataset.sessionId
-          })
-          .then(function (result) {
-            if (result.error) {
-              // If `redirectToCheckout` fails due to a browser or network
-              // error, display the localized error message to your customer.
-              let displayError = document.getElementById('error-message');
-              displayError.textContent = result.error.message;
-            }
-          });
-        });
-      });
-
       const event = new Event('click');
       stripeButton.dispatchEvent(event);
     } else {

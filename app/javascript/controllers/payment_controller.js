9ab441cd7f2f72a5be984de2b8682fcfc7df093f
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-11-25 10:56:07 +0100

    details

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -8,8 +8,8 @@ export default class extends Controller {
   }
 
   toggleSubscription (event) {
-    // event.preventDefault();
     event.stopImmediatePropagation();
+    // event.preventDefault();
 
     const sessionID = event.currentTarget.dataset.sessionId;
     const stripeButton = document.querySelector(`.button-${sessionID}`);
@@ -19,6 +19,24 @@ export default class extends Controller {
     if (event.currentTarget === cgvCheckbox) {
       cgvCheckbox.checked = !cgvCheckbox.checked;
     } else if (event.currentTarget === stripeButton && cgvCheckbox.checked === true) {
+      let stripe = Stripe("<%= ENV["STRIPE_PUBLISHABLE_KEY"] %>");
+      let checkoutButtons = document.querySelectorAll('.checkout-button');
+      checkoutButtons.forEach(checkoutButton => {
+        checkoutButton.addEventListener('click', function () {
+          stripe.redirectToCheckout({
+            sessionId: checkoutButton.dataset.sessionId
+          })
+          .then(function (result) {
+            if (result.error) {
+              // If `redirectToCheckout` fails due to a browser or network
+              // error, display the localized error message to your customer.
+              let displayError = document.getElementById('error-message');
+              displayError.textContent = result.error.message;
+            }
+          });
+        });
+      });
+
       const event = new Event('click');
       stripeButton.dispatchEvent(event);
     } else {

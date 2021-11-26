d023e8aa3e5f99c8b8466b8c8214820d0197182a
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-11-26 12:45:31 +0100

    new payment is ok for CGV checkbox

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ['button']
-
-  toggle() {
-    this.buttonTarget.classList.toggle('hidden');
-  }
+  static targets = []
 
   toggleSubscription (event) {
     event.stopImmediatePropagation();

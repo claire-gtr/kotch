40988b8a70797146b24ee96cc4524ac3c73fdd7f
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-12-01 12:02:26 +0100

    feat(partner#model): remove empty spaces in coupon code and domaine name before save

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,8 @@
 class Partner < ApplicationRecord
+  before_save :remove_empty_spaces
+
+  def remove_empty_spaces
+    self.domain_name = self.domain_name.gsub(/\s+/, '')
+    self.coupon_code = self.coupon_code.gsub(/\s+/, '')
+  end
 end

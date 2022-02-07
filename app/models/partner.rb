a4b24d9956ba64dd593469c55b0edb8625823362
Bertrand Bichat <bertrand.bichat@gmail.com>
2022-02-07 16:46:45 +0100

    add validation to partner model

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,6 +1,11 @@
 class Partner < ApplicationRecord
   before_save :remove_empty_spaces
 
+  validates :coupon_code, presence: true, uniqueness: true
+  validates :domain_name, presence: true, uniqueness: true
+  validates :percentage, presence: true
+  validates :name, presence: true
+
   def remove_empty_spaces
     self.domain_name = self.domain_name.gsub(/\s+/, '')
     self.coupon_code = self.coupon_code.gsub(/\s+/, '')
